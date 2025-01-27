613f1e738af8ac433cfc9b9c890884742c976d66
Michal Maciejewski <maciejewskimichal7@gmail.com>
2025-01-28 00:21:05 +0100

    feat(quiz): add basic views

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/create-quiz',
+      name: 'create-quiz',
+      component: () => import('../views/create-quiz/CreateQuizView.vue')
+    },
   ],
 })
 
---
+++ src/views/create-quiz/CreateQuizView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h3>hello form quiz create</h3>
+    <CreateAnswer />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import CreateAnswer from './CreateAnswer.vue'
+
+</script>
+
+<style scoped></style>
